47de336d278d51f9905a06c3dd485511a7f8ed0d
tangjie <jietang.witcet@gmail.com>
2024-11-19 16:20:57 +0800

    add more info

--- beacon/client.go
+++ beacon/client.go
@@ -9,7 +9,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/DillLabs/dillscan-rewards/src/common/log"
 	"github.com/DillLabs/dillscan-rewards/src/common/util/http/client"
 	"github.com/DillLabs/eth-rewards/types"
 )
@@ -35,15 +34,14 @@ func (c *Client) Balance(slot uint64, validator uint64) (uint64, error) {
 
 	// resp, err := c.httpClient.Get(url)
 
-	resp, err := client.RetryWithDefaults(c.httpClient, func() (*http.Response, error) {
+	resp, err := client.RetryWithDefaults(func() (*http.Response, error) {
 		return c.httpClient.Get(url)
-	}, "Balance")
+	}, fmt.Sprintf("Balance for slot %d (epoch %d)", slot, slot/32))
 
 	if err != nil {
 		return 0, err
 	}
 	defer resp.Body.Close()
-
 	if resp.StatusCode != 200 {
 		return 0, fmt.Errorf("http request error: %s", resp.Status)
 	}
@@ -63,14 +61,14 @@ func (c *Client) AttestationRewards(epoch uint64) (*types.AttestationRewardsApiR
 	url := fmt.Sprintf("%s/eth/v1/beacon/rewards/attestations/%d", c.endpoint, epoch)
 	data := []byte("[]") //request data for all validators
 
-	resp, err := client.RetryWithDefaults(c.httpClient, func() (*http.Response, error) {
+	resp, err := client.RetryWithDefaults(func() (*http.Response, error) {
 		return c.httpClient.Post(url, "application/json", bytes.NewReader(data))
-	}, "AttestationRewards")
+	}, fmt.Sprintf("AttestationRewards for (epoch %d)", epoch))
+
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-
 	if resp.StatusCode != 200 {
 		return nil, fmt.Errorf("http request error: %s", resp.Status)
 	}
@@ -89,24 +87,14 @@ func (c *Client) SyncCommitteeRewards(slot uint64) (*types.SyncCommitteeRewardsA
 	url := fmt.Sprintf("%s/eth/v1/beacon/rewards/sync_committee/%d", c.endpoint, slot)
 	data := []byte("[]") //request data for all validators
 
-	startTime := time.Now()
-
-	resp, err := client.RetryWithDefaults(c.httpClient, func() (*http.Response, error) {
+	resp, err := client.RetryWithDefaults(func() (*http.Response, error) {
 		return c.httpClient.Post(url, "application/json", bytes.NewReader(data))
-	}, "SyncCommitteeRewards")
-	endTime := time.Now()
-	duration := endTime.Sub(startTime).Seconds()
-	log.Debug("in SyncCommitteeRewards duration", "slot", slot, "duration", duration)
+	}, fmt.Sprintf("SyncCommitteeRewards for slot %d (epoch %d)", slot, slot/32))
+
 	if err != nil {
-		log.Error("SyncCommitteeRewards", "err", err)
 		return nil, err
 	}
-	// tj added
-	if resp == nil {
-		return nil, fmt.Errorf("received nil response %s", strconv.FormatUint(slot, 10))
-	}
 	defer resp.Body.Close()
-
 	if resp.StatusCode != 200 {
 		if resp.StatusCode == 404 {
 			return nil, types.ErrBlockNotFound
@@ -129,26 +117,15 @@ func (c *Client) SyncCommitteeRewards(slot uint64) (*types.SyncCommitteeRewardsA
 
 func (c *Client) BlockRewards(slot uint64) (*types.BlockRewardsApiResponse, error) {
 	url := fmt.Sprintf("%s/eth/v1/beacon/rewards/blocks/%d", c.endpoint, slot)
-	startTime := time.Now()
 
-	resp, err := client.RetryWithDefaults(c.httpClient, func() (*http.Response, error) {
+	resp, err := client.RetryWithDefaults(func() (*http.Response, error) {
 		return c.httpClient.Get(url)
-	}, "BlockRewards")
+	}, fmt.Sprintf("BlockRewards for slot %d (epoch %d)", slot, slot/32))
 
-	endTime := time.Now()
-	duration := endTime.Sub(startTime).Seconds()
-	log.Debug("in BlockRewards duration", "slot", slot, "duration", duration)
 	if err != nil {
-		log.Error("BlockRewards", "err", err)
 		return nil, err
 	}
-
-	if resp == nil {
-		return nil, fmt.Errorf("received nil response %s", strconv.FormatUint(slot, 10))
-	}
-
 	defer resp.Body.Close()
-
 	if resp.StatusCode != 200 {
 		if resp.StatusCode == 404 {
 			return nil, types.ErrBlockNotFound
@@ -169,9 +146,9 @@ func (c *Client) BlockRewards(slot uint64) (*types.BlockRewardsApiResponse, erro
 func (c *Client) ProposerAssignments(epoch uint64) (*types.EpochProposerAssignmentsApiResponse, error) {
 	url := fmt.Sprintf("%s/eth/v1/validator/duties/proposer/%d", c.endpoint, epoch)
 
-	resp, err := client.RetryWithDefaults(c.httpClient, func() (*http.Response, error) {
+	resp, err := client.RetryWithDefaults(func() (*http.Response, error) {
 		return c.httpClient.Get(url)
-	}, "ProposerAssignments")
+	}, fmt.Sprintf("ProposerAssignments for (epoch %d)", epoch))
 
 	if err != nil {
 		return nil, err
@@ -196,9 +173,9 @@ func (c *Client) ProposerAssignments(epoch uint64) (*types.EpochProposerAssignme
 func (c *Client) ExecutionBlockNumber(slot uint64) (uint64, error) {
 	url := fmt.Sprintf("%s/eth/v2/beacon/blocks/%d", c.endpoint, slot)
 
-	resp, err := client.RetryWithDefaults(c.httpClient, func() (*http.Response, error) {
+	resp, err := client.RetryWithDefaults(func() (*http.Response, error) {
 		return c.httpClient.Get(url)
-	}, "ExecutionBlockNumber")
+	}, fmt.Sprintf("ExecutionBlockNumber for slot %d (epoch %d)", slot, slot/32))
 
 	if err != nil {
 		return 0, err
